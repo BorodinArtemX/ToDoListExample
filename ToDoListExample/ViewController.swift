1ea3f59130d1e9a04b7e140390e488f38be7f404
BorodinArtemX <BorodinArtemX@icloud.com>
2023-03-30 16:52:06 +0200

    Starting Project

---
+++ ToDoListExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListExample
+//
+//  Created by Артем Бородин on 30/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
